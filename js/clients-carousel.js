cb90f9e295c0fdb2d78903fc035d0e192ccebc66
NeuralNat <ifrance@yandex.ru>
2024-01-20 23:38:04 +0300

    JS custom silder button click listen

--- js/clients-carousel.js
+++ js/clients-carousel.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-    $("#clients-slider").owlCarousel({
-        items: 1
+
+    // Находим блок с слайдерами
+    const owl = $('#clients-slider');
+    // Запускаем карусель
+    owl.owlCarousel({
+        items: 1,
+        loop: true
+    });
+
+    // Находим кастомные кнопки вперед/назад
+    const prev = $('#sliderPrev');
+    const next = $('#sliderNext');
+
+    // Клик на кнопку назад и прокрутка карусели
+    prev.click(function(){
+        owl.trigger('prev.owl.carousel')
+    });
+
+    // Клик на кнопку вперед и прокрутка карусели
+    next.click(function(){
+        owl.trigger('next.owl.carousel')
     });
   });
